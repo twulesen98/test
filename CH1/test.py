218c93f44984aa841c0b267ef449b960172ee958
twulesen98 <twulesen98@gmail.com>
2021-11-10 15:11:45 +0800

    CH1

--- CH1/test.py
+++ CH1/test.py
@@ -4,3 +4,4 @@ radius_int = int(radius_str)
 circumference = 2 * math.pi * radius_int
 area = math.pi * (radius_int**2)
 print('The circumference is: ', circumference, ', and the area is: ', area)
+
