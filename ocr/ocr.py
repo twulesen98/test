1573f58c91718b79b524ce0184ec1cd88a06f29b
twulesen98 <twulesen98@gmail.com>
2022-01-19 15:47:37 +0800

    python

--- ocr/ocr.py
+++ ocr/ocr.py
@@ -1,7 +1,9 @@
+import pytesseract
+from PIL import Image
 import cv2
-img = cv2.imread("./ocr/jit.jepg",cv2.IMREAD_COLOR)
-
+ 
+img = cv2.imread("./app/jit.jpeg",cv2.IMREAD_COLOR)
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
 gray = cv2.bilateralFilter(gray, 11, 17, 17) 
- 
 text = pytesseract.image_to_string(gray, 'jpn')
+print(text)
