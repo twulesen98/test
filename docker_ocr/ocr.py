74eba3a351e61898fd1aa86b23dfb520fe69777d
twulesen98 <twulesen98@gmail.com>
2022-01-19 16:03:08 +0800

    python

--- docker_ocr/ocr.py
+++
@@ -1,9 +0,0 @@
-import pytesseract
-from PIL import Image
-import cv2
- 
-img = cv2.imread("./app/jit.jpeg",cv2.IMREAD_COLOR)
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
-gray = cv2.bilateralFilter(gray, 11, 17, 17) 
-text = pytesseract.image_to_string(gray, 'jpn')
-print(text)
